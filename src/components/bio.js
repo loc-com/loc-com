81a3bc58251b9e89f066a5abe29d8077990c6eb3
deathemperor <deathemperor@gmail.com>
2021-05-25 18:20:14 +0700

    fix facebook link (FINALLY)

--- src/components/bio.js
+++ src/components/bio.js
@@ -19,7 +19,7 @@ const Bio = () => {
             summary
           }
           social {
-            twitter
+            facebook
           }
         }
       }
@@ -29,6 +29,7 @@ const Bio = () => {
   // Set these values by editing "siteMetadata" in gatsby-config.js
   const author = data.site.siteMetadata?.author
   const social = data.site.siteMetadata?.social
+  console.log(social)
 
   return (
     <div className="bio">
